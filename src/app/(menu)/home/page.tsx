8f108363304a50b7bb52e546e281630d3e9ebc53
takecchi <takecchi.kobayashi@gmail.com>
2023-09-08 01:27:31 +0900

    Post周りのコンポーネント作成

--- src/app/(menu)/home/page.tsx
+++ src/app/(menu)/home/page.tsx
@@ -1,12 +1,8 @@
 'use client';
 
 import Post from '@/components/timeline/Post';
-import { useState } from 'react';
 
 export default function Home() {
-  const [favorited, setFavorite] = useState(false);
-  const [reposted, setRepost] = useState(false);
-
   return (
     <main style={{ height: '2000px' }}>
       <Post
@@ -17,9 +13,7 @@ export default function Home() {
         postId={1}
         postedAt={new Date()}
         replyCount={0}
-        favorited={favorited}
         favoriteCount={0}
-        reposted={reposted}
         repostCount={0}
       />
     </main>
