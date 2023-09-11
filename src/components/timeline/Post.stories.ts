ad01ddbd31714ff2e375120fdb138c7c4cb8d485
takecchi <takecchi.kobayashi@gmail.com>
2023-09-11 21:37:56 +0900

    storybookで動くように

--- src/components/timeline/Post.stories.ts
+++ src/components/timeline/Post.stories.ts
@@ -13,6 +13,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const NormalPost: Story = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
   args: {
     displayName: 'ククルス',
     userName: 'cuculus',
