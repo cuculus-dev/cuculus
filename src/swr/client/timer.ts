c3ac27d1364b0536be919a9bfdcccf8e72518af3
takecchi <takecchi.kobayashi@gmail.com>
2023-09-13 11:08:04 +0900

    useTimerとして使用できる値を1秒と1分に制限

--- src/swr/client/timer.ts
+++ src/swr/client/timer.ts
@@ -4,7 +4,7 @@ const fetcher = () => {
   return new Date();
 };
 
-const useTimer = (interval: number) => {
+const useTimer = (interval: 60000 | 1000) => {
   return useSWR<Date>(`useTimer:${interval}`, fetcher, {
     refreshInterval: interval,
     dedupingInterval: interval,
