94aa426a531f70824dbc3ce12fbddd95b064716c
kawanolk <startNN3014@gmail.com>
2023-09-17 22:29:18 +0900

    仮作成の更新

--- src/components/common/atoms/LoginForm.tsx
+++ src/components/common/atoms/LoginForm.tsx
@@ -1,22 +1,34 @@
+import FormButton from '@/components/common/atoms/FormButton';
 import LinkButton from '@/components/common/atoms/LinkButton';
+import { Grid, styled } from '@mui/material';
+
+const StyledLogin = styled('div');
+
+type FormInputProps = {
+  id: string;
+  password: string;
+};
 
 export default function LoginForm() {
   return (
     <form>
+      {/* タイトル */}
       <div>サインイン</div>
       <div>アカウント</div>
       {/* 入力欄 */}
-      <input type="text" placeholder="ユーザー名またはeメールアドレス"></input>
+      <input id="id" type="text" placeholder="ユーザー名またeメールアドレス" />
       <div>
-        <input type="text" placeholder="パスワード"></input>
+        <input id="password" type="password" placeholder="パスワード" />
         {/* リンク */}
         {/* 更新画面へのリンク */}
-        <LinkButton href={'/'}>忘れた</LinkButton>
+        {/* <LinkButton href={'/'}>忘れた</LinkButton> */}
       </div>
-      {/*入力を破棄してTopへ*/}
+      {/*入力を保持せずしてTopへ*/}
       <LinkButton href={'/'}>戻る</LinkButton>
-      {/* ここで情報送信 */}
-      <LinkButton href={'/'}>サインイン</LinkButton>
+      {/* 入力情報送信 */}
+      <FormButton href={'/login'} type="submit">
+        サインイン
+      </FormButton>
     </form>
   );
 }
