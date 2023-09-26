fcc3f3777597f7069b8c1c99041697c469c6ef4b
takecchi <takecchi.kobayashi@gmail.com>
2023-09-27 02:07:48 +0900

    本番と条件を合わせる

--- proxy.js
+++ proxy.js
@@ -31,10 +31,6 @@ proxy.use(
     },
     onProxyRes: (proxyRes) => {
       proxyRes.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000';
-      proxyRes.headers['Access-Control-Allow-Methods'] =
-        'GET, POST, PUT, DELETE, OPTIONS';
-      proxyRes.headers['Access-Control-Allow-Headers'] =
-        'Content-Type, Authorization, x-id, Content-Length, X-Requested-With';
       proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
     },
   }),
