8d23c1c11c4c1b7b10d02eeb178c9472ebc3d055
takecchi <takecchi.kobayashi@gmail.com>
2023-09-29 02:34:27 +0900

    テストとはいえTimelineの再レンダリングが遅かったのでuseMemo化

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 import Post from '@/components/timeline/Post';
 
 /**
@@ -6,19 +6,22 @@ import Post from '@/components/timeline/Post';
  * @constructor
  */
 export default function Timeline() {
-  const posts: ReactNode[] = [];
-  for (let i = 0; i < 100; i++) {
-    posts.push(
-      <Post
-        displayName={'ククルス'}
-        userName={'cuculus'}
-        profileImageUrl={'/'}
-        text={'あああああああああああああああああああ'}
-        postId={1}
-        postedAt={new Date()}
-        replyCount={0}
-      />,
-    );
-  }
+  const posts: ReactNode[] = useMemo(() => {
+    const tempPosts: ReactNode[] = [];
+    for (let i = 0; i < 10; i++) {
+      tempPosts.push(
+        <Post
+          displayName={'ククルス'}
+          userName={'cuculus'}
+          profileImageUrl={'/'}
+          text={'あああああああああああああああああああ'}
+          postId={1}
+          postedAt={new Date()}
+          replyCount={0}
+        />,
+      );
+    }
+    return tempPosts;
+  }, []);
   return <div>{posts}</div>;
 }
