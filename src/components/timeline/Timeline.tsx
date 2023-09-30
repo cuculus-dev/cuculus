1773c9c64b0d38c03deffe08cad2dc70ff864ab7
takecchi <takecchi.kobayashi@gmail.com>
2023-09-30 20:06:44 +0900

    タイムラインのモックを20件固定に

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,29 +1,26 @@
 'use client';
 
-import { useTimelinePrevious } from '@/swr/client/timeline';
+import { useTimeline } from '@/swr/client/timeline';
 import TimelinePost from '@/components/timeline/organisms/TimelinePost';
 
 /**
  * FIXME THIS IS MOCK
- * さらに読み込む度に既存コンポーネントにも再レンダリングが走るので要調整
+ * 再レンダリング起こすタイミングは要調整
  * @constructor
  */
 export default function Timeline() {
-  const { data, setSize, size } = useTimelinePrevious();
+  const { data } = useTimeline();
   return (
     <div>
-      {data?.map((posts) => {
-        return posts.map((post) => {
-          return (
-            <TimelinePost
-              key={post.postId}
-              postId={post.postId}
-              fallbackData={post}
-            />
-          );
-        });
+      {data?.map((post) => {
+        return (
+          <TimelinePost
+            key={post.postId}
+            postId={post.postId}
+            fallbackData={post}
+          />
+        );
       })}
-      <button onClick={() => void setSize(size + 1)}>さらに読み込む</button>
     </div>
   );
 }
