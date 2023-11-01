6f33955eadaf1cc5c28da4cff88d7f50ca33edc2
takecchi <takecchi.kobayashi@gmail.com>
2023-11-01 22:38:02 +0900

    robots.txtとsitemaps.xmlの実装

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,35 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
+  // Development robots.txt
+  if (process.env.NODE_ENV !== 'production') {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: ['/'],
+      },
+    };
+  }
+  // Production robots.txt
   return {
-    rules: {
-      userAgent: '*',
-      allow: ['/'],
-    },
-    sitemap: process.env.SITE_URL + '/sitemap.xml',
+    rules: [
+      {
+        userAgent: 'Yahoo Pipes 1.0',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'Livelapbot',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'Megalodon',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'ia_archiver',
+        disallow: ['/'],
+      },
+    ],
+    sitemap: process.env.SITE_URL + '/sitemaps.xml',
   };
 }
