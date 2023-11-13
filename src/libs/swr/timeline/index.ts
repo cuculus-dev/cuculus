60a98674b2bedc628175b46cc620e62e62af368b
takecchi <takecchi.kobayashi@gmail.com>
2023-11-13 15:57:47 +0900

    swrミドルウェアの再設計

--- src/libs/swr/timeline/index.ts
+++ src/libs/swr/timeline/index.ts
@@ -1,199 +1,315 @@
-import useSWR, { BareFetcher, Middleware } from 'swr';
+import useSWR, { BareFetcher, Middleware, SWRConfig } from 'swr';
 import {
   createCacheHelper,
   Key,
   serialize,
+  SWRGlobalState,
   SWRResponse,
   withMiddleware,
+  cache as defaultCache,
+  GlobalState,
+  getTimestamp,
 } from 'swr/_internal';
 import {
+  FetchResultData,
   Gap,
-  SWRTimelineCacheValue,
+  ResultDataCacheValue,
   SWRTimelineConfiguration,
   SWRTimelineFetcher,
   SWRTimelineHook,
+  SWRTimelineKeyLoader,
   SWRTimelineResponse,
-  Timeline,
+  TimelineData,
+  TimelineDataCacheValue,
 } from '@/libs/swr/timeline/types';
 import { useCallback } from 'react';
-import { serializeGap } from '@/libs/swr/timeline/serialize';
+import useSWRImmutable from 'swr/immutable';
+import { findLatest, findOldest } from '@/libs/swr/timeline/utils';
 
-const findLatest = <Data>(timeline: Timeline<Data>): Data | undefined => {
-  const data = timeline.find((data) => Array.isArray(data)) as
-    | Data[]
-    | undefined;
-  if (data && data.length > 0) {
-    return data[0];
-  }
-  return undefined;
-};
-
-const findGap = <Data>(timeline: Timeline<Data>, gap: Gap<Data>): number => {
-  const key = serializeGap(gap);
-  return timeline.findIndex((data) => {
-    if (!Array.isArray(data)) {
-      return key === serializeGap(data);
-    }
-  });
-};
-
-const findOldest = <Data>(timeline: Timeline<Data>): Data | undefined => {
-  let data: Data[] | undefined;
-  for (let i = timeline.length - 1; i >= 0; i--) {
-    if (Array.isArray(timeline[i])) {
-      data = timeline[i] as Data[];
-      break;
-    }
-  }
-  if (data && data.length > 0) {
-    return data[data.length - 1];
-  }
-  return undefined;
-};
+const TIMELINE_PREFIX = '$timeline$';
+const QUEUE_PREFIX = '$queue$';
 
 /**
  * 専用SWRHook
  */
 interface SWRHook<T> {
   //eslint-disable-next-line @typescript-eslint/no-explicit-any
-  <Data = Timeline<T>, Error = any>(
-    key: Key,
+  <Data = TimelineData<T>, Error = any, SWRKey extends Key = Key>(
+    key: SWRKey,
     fetcher: BareFetcher<Data> | null,
     //eslint-disable-next-line @typescript-eslint/no-explicit-any
     config?: any,
   ): SWRResponse<Data, Error>;
 }
 
-const timeline = (<Data, Error>(useSWRNext: SWRHook<Data>) =>
+const timeline = (<Data, Error, SWRKey extends Key = Key>(
+    useSWRNext: SWRHook<Data>,
+  ) =>
   (
-    _key: Key,
-    fetcher: SWRTimelineFetcher<Data> | null,
-    config: SWRTimelineConfiguration<Data, Error>,
+    getKey: SWRTimelineKeyLoader<Data, SWRKey>,
+    fetcher: SWRTimelineFetcher<Data, SWRKey> | null,
+    config: Omit<typeof SWRConfig.defaultValue, 'fetcher'> &
+      Omit<SWRTimelineConfiguration<Data, Error>, 'fetcher'>,
   ): SWRTimelineResponse<Data, Error> => {
-    const { cache } = config;
-    const [key] = serialize(_key);
-    const [get, set] = createCacheHelper<Data, SWRTimelineCacheValue<Data>>(
-      cache,
-      key,
-    );
+    // be Immutable
+    config.revalidateOnFocus = false;
+    config.revalidateIfStale = false;
+    config.revalidateOnReconnect = false;
 
-    const swr: SWRResponse<Timeline<Data> | undefined, Error> = useSWRNext(
-      key,
-      async (): Promise<Timeline<Data> | undefined> => {
-        // 前回のデータ
-        const cacheData = get().data;
-        if (cacheData && cacheData.length > 0) {
-          set({ isValidating: false });
-          return cacheData;
-        }
-        // 初回のデータ
-        if (fetcher) {
-          const result = await fetcher(null, null);
-          if (result) {
-            return result.data.length > 0 ? [result.data] : [];
-          }
-        }
-      },
-      {
-        ...config,
-        revalidateOnFocus: false,
-        revalidateIfStale: false,
-        revalidateOnReconnect: false,
-      },
-    );
+    const { cache, enableQueue = false } = config;
+    const [, , FETCH] = SWRGlobalState.get(defaultCache) as GlobalState;
 
-    // mutateLatest
-    const mutateLatest = useCallback(
-      async (data?: Timeline<Data>): Promise<Timeline<Data> | undefined> => {
-        const cache = get();
-        const cacheData = cache.data;
-        if (cacheData && !cache.isValidating) {
-          const latest = findLatest<Data>(cacheData);
-          if (latest && fetcher) {
-            set({ isValidating: true });
-            if (data && Array.isArray(data)) {
-              return swr.mutate([...data, ...cacheData]);
+    let timelineKey: string | undefined;
+    let queueKey: string | undefined;
+    try {
+      const serialized = serialize(getKey(null, null))[0];
+      if (serialized) {
+        timelineKey = TIMELINE_PREFIX + serialized;
+        queueKey = QUEUE_PREFIX + serialized;
+      }
+    } catch (err) {
+      // Not ready yet.
+    }
+
+    /**
+     * キー値から再検証を行い、検証結果を返す
+     */
+    const validateKeys = useCallback(
+      async (_keys: SWRKey[]): Promise<[SWRKey[], TimelineData<Data>]> => {
+        const data: TimelineData<Data> = [];
+        const keys: SWRKey[] = [];
+
+        let prevOldest: Data | undefined;
+
+        // 前回のキャッシュキーから取得
+        for (const _key of _keys) {
+          // 次のループに引き継ぐフラグ
+          let possiblyHasGap = false;
+          const [pageKey, pageArg] = serialize(_key);
+
+          const [getSWRCache, setSWRCache] = createCacheHelper<
+            Data,
+            ResultDataCacheValue<Data>
+          >(cache, pageKey);
+
+          let pageData = getSWRCache().data;
+          // 再検証フラグ
+          const shouldFetchPage = pageData === undefined;
+          if (fetcher && shouldFetchPage) {
+            // 実行中のfetchがないか確認
+            if (!(pageKey in FETCH)) {
+              FETCH[pageKey] = [fetcher(_key), getTimestamp()];
             }
-            const fetched = data ?? (await fetcher(latest, null));
-            if (fetched && fetched.data.length > 0) {
-              const newData = [...cacheData];
-              if (fetched.hasGap && fetched.data.length > 0) {
-                newData.unshift({
-                  since: latest,
-                  max: fetched.data[fetched.data.length - 1],
-                });
+            let resultData: FetchResultData<Data> | undefined;
+            try {
+              resultData = (await FETCH[pageKey][0]) as
+                | FetchResultData<Data>
+                | undefined;
+            } finally {
+              delete FETCH[pageKey];
+            }
+
+            // 空だった場合は何もしない
+            if (resultData && resultData?.data.length > 0) {
+              pageData = resultData?.data;
+              setSWRCache({ data: pageData, _k: pageArg });
+
+              // 次のループに引き継ぐフラグを立てる
+              possiblyHasGap = resultData?.possiblyHasGap ?? false;
+            }
+          }
+
+          // undefinedだった場合はなかったことにする
+          if (pageData) {
+            if (Array.isArray(pageData)) {
+              // 前回のループで取得漏れがあった場合はここで処理を行う
+              if (prevOldest) {
+                // gap追加処理
+                if (pageData.length > 0) {
+                  const gap: Gap<Data> = {
+                    since: pageData[0],
+                    max: prevOldest,
+                  };
+                  const gapHistory: SWRKey = getKey(gap.since, gap.max);
+                  const [gapKey, gapArg] = serialize(gapHistory);
+                  const [, setGapCache] = createCacheHelper<
+                    Data,
+                    ResultDataCacheValue<Data>
+                  >(cache, gapKey);
+                  setGapCache({ data: gap, _k: gapArg });
+                  data.push(gap);
+                  keys.push(gapHistory);
+                }
+                // reset
+                prevOldest = undefined;
               }
-              newData.unshift(fetched.data);
-              return swr.mutate(newData);
+
+              // 今回取得漏れが発生していた場合、次回に引き継ぐ
+              if (possiblyHasGap && pageData.length > 0) {
+                prevOldest = pageData[pageData.length - 1];
+              }
+            }
+
+            // 追加
+            data.push(pageData);
+            keys.push(_key);
+          }
+        }
+        return [keys, data];
+      },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [cache],
+    );
+
+    const [, set] = createCacheHelper<
+      Data,
+      TimelineDataCacheValue<Data, Error, SWRKey>
+    >(cache, timelineKey);
+
+    const [getQueue, setQueue] = createCacheHelper<
+      Data,
+      Omit<TimelineDataCacheValue<Data, Error, SWRKey>, '_trigger'>
+    >(cache, queueKey);
+
+    const swrQueue: SWRResponse<TimelineData<Data> | undefined, Error> =
+      useSWRImmutable(
+        queueKey,
+        async (key: string): Promise<TimelineData<Data> | undefined> => {
+          const [getCache, setCache] = createCacheHelper<
+            Data,
+            Omit<TimelineDataCacheValue<Data, Error, SWRKey>, '_trigger'>
+          >(cache, key);
+
+          // 前回のキャッシュキー
+          const _keys = getCache()._keys;
+          if (_keys) {
+            const [keys, data] = await validateKeys(_keys);
+            setCache({ _keys: keys });
+            return data;
+          }
+          return [];
+        },
+      );
+
+    // Actual SWR hook to load all pages in one fetcher.
+    const swr: SWRResponse<TimelineData<Data> | undefined, Error> = useSWRNext(
+      timelineKey,
+      async (key: string): Promise<TimelineData<Data> | undefined> => {
+        const [getCache, setCache] = createCacheHelper<
+          Data,
+          TimelineDataCacheValue<Data, Error, SWRKey>
+        >(cache, key);
+
+        // 前回のキャッシュキー
+        let _keys = getCache()._keys;
+
+        // ない場合は初期値を入れる
+        if (_keys === undefined || _keys.length === 0) {
+          setCache({ _keys: [getKey(null, null)] });
+          _keys = getCache()._keys;
+        } else {
+          // 初期化が終わっている場合
+          const _trigger = getCache()._trigger;
+          const cacheData = getCache().data;
+
+          if (_trigger === 'older') {
+            // cacheから最古のDataを取得
+            const oldest = cacheData ? findOldest(cacheData) : undefined;
+            if (oldest) {
+              _keys = [..._keys, getKey(null, oldest)];
+            }
+          }
+
+          // キューの更新(基本的にはここで更新は起こらない想定)
+          let queueData: TimelineData<Data> | undefined;
+          if ((getQueue()._keys?.length ?? 0) > 0) {
+            queueData = await swrQueue.mutate();
+          }
+
+          // queueとcacheから最新の投稿を取得
+          let latest = queueData ? findLatest(queueData) : undefined;
+          if (latest === undefined) {
+            latest = cacheData ? findLatest(cacheData) : undefined;
+          }
+
+          // latestが特定できた場合
+          if (latest) {
+            const queueKeys = getQueue()._keys;
+            const latestKey = getKey(latest, null);
+            const hasQueue = queueKeys && queueKeys.length > 0;
+
+            if (_trigger === undefined && enableQueue) {
+              // 最新のデータをキューに取得させる
+              if (hasQueue) {
+                setQueue({ _keys: [latestKey, ...queueKeys] });
+              } else {
+                setQueue({ _keys: [latestKey] });
+              }
+              await swrQueue.mutate();
+            } else if (!enableQueue || _trigger === 'latest') {
+              // キューのデータを移し替える
+              if (hasQueue) {
+                _keys = [...queueKeys, ..._keys];
+                setQueue({ _keys: [] });
+                await swrQueue.mutate();
+              }
+              // 最新のデータ取得指示
+              _keys = [latestKey, ..._keys];
             }
           }
         }
+
+        // トリガーのリセット
+        setCache({ _trigger: undefined });
+
+        if (_keys) {
+          const [keys, data] = await validateKeys(_keys);
+          setCache({ _keys: keys });
+          return data;
+        }
+
+        return [];
+      },
+      config,
+    );
+
+    // 最新の投稿を読み込む
+    const mutateLatest = useCallback(
+      async (): Promise<TimelineData<Data> | undefined> => {
+        set({ _trigger: 'latest' });
         return swr.mutate();
       },
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      [key, cache],
+      [timelineKey, cache],
+    );
+
+    // 過去の投稿を読み込む
+    const mutateOlder = useCallback(
+      async (): Promise<TimelineData<Data> | undefined> => {
+        set({ _trigger: 'older' });
+        return swr.mutate();
+      },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [timelineKey, cache],
     );
 
     // mutateGap
     const mutateGap = useCallback(
-      async (gap: Gap<Data>): Promise<Timeline<Data> | undefined> => {
-        const cache = get();
-        const cacheData = cache.data;
-        if (cacheData && !cache.isValidating) {
-          const index = findGap<Data>(cacheData, gap);
-          if (index >= 0 && fetcher) {
-            set({ isValidating: true });
-            const fetched = await fetcher(gap.since, gap.max);
-            /**
-             * パターン
-             * [] + true ... 何もしない
-             * [] + false ... 消す
-             * [1,2,3] + true ... 追加されgapが生まれる
-             * [1,2,3] + false ... 追加されgapが消える
-             */
-            if (fetched) {
-              const newData = [...cacheData];
-              if (fetched.data.length > 0) {
-                if (fetched.hasGap) {
-                  newData.splice(index, 1, fetched.data, {
-                    since: gap.since,
-                    max: fetched.data[fetched.data.length - 1],
-                  });
-                } else {
-                  newData.splice(index, 1, fetched.data);
-                }
-              } else if (!fetched.hasGap) {
-                newData.splice(index, 1);
-              }
-              return swr.mutate(newData);
-            }
-          }
+      async (gap: Gap<Data>): Promise<TimelineData<Data> | undefined> => {
+        const gapHistory: SWRKey = getKey(gap.since, gap.max);
+        const [gapKey, gapArg] = serialize(gapHistory);
+        const [getGapCache, setGapCache] = createCacheHelper<
+          Data,
+          ResultDataCacheValue<Data>
+        >(cache, gapKey);
+        if (getGapCache().data) {
+          setGapCache({ data: undefined, _k: gapArg });
         }
+        set({ _trigger: 'gap' });
         return swr.mutate();
       },
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      [key, cache],
-    );
-
-    // mutateOlder
-    const mutateOlder = useCallback(
-      async (data?: Data[]): Promise<Timeline<Data> | undefined> => {
-        const cache = get();
-        const cacheData = cache.data;
-        if (cacheData && !cache.isValidating) {
-          const oldest = findOldest<Data>(cacheData);
-          if (oldest && fetcher) {
-            set({ isValidating: true });
-            const fetched = data ?? (await fetcher(null, oldest))?.data;
-            if (fetched && fetched.length > 0) {
-              return swr.mutate([...cacheData, fetched]);
-            }
-          }
-        }
-        return swr.mutate();
-      },
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      [key, cache],
+      [timelineKey, cache],
     );
 
     return {
@@ -212,6 +328,9 @@ const timeline = (<Data, Error>(useSWRNext: SWRHook<Data>) =>
       mutateLatest,
       mutateOlder,
       mutateGap,
+      get queue() {
+        return swrQueue.data;
+      },
     };
   }) as unknown as Middleware;
 
--- src/libs/swr/timeline/types.ts
+++ src/libs/swr/timeline/types.ts
@@ -1,88 +1,82 @@
-import {
-  Arguments,
-  FullConfiguration,
-  Key,
-  State,
-  SWRResponse,
-} from 'swr/_internal';
+import { Arguments, State, SWRConfiguration, SWRResponse } from 'swr/_internal';
 
-/**
- * 取得漏れの可能性があるデータの範囲
- */
+export type SWRTimelineKeyLoader<
+  Data = any,
+  SWRKey extends Arguments = Arguments,
+> = (since: Data | null, max: Data | null) => SWRKey;
+
 export type Gap<Data> = {
   since: Data;
   max: Data;
 };
 
 /**
- * 返却されるdataの型
- */
-export type Timeline<Data> = Array<Data[] | Gap<Data>>;
-
-/**
- * fetcherに返却して欲しい型
- */
-export type TimelineResult<Data> = {
-  data: Data[];
-  hasGap: boolean;
-};
-
-/**
  * fetcherの返却値
  */
-type FetcherResponse<Data> = Promise<TimelineResult<Data>> | Promise<undefined>;
+export type FetchResultData<Data> = {
+  data: Data[];
+  possiblyHasGap: boolean; // データを取得し切れなかった可能性がある
+};
+/**
+ * fetch結果
+ */
+export type ResultData<Data> = Data[] | Gap<Data>; // フェッチ単位
+/**
+ * fetch結果のまとめ
+ */
+export type TimelineData<Data> = ResultData<Data>[]; // Timeline
 
 /**
  * fetcherの型
  */
-export type SWRTimelineFetcher<Data> = (
-  since: Data | null,
-  max: Data | null,
-) => FetcherResponse<Data>;
+export type SWRTimelineFetcher<Data, SWRKey extends Arguments> = (
+  arg: SWRKey,
+) => Promise<FetchResultData<Data> | undefined>;
 
-export interface SWRTimelineResponse<Data = any, Error = any>
+export interface SWRTimelineResponse<Data = any, Error = never>
   extends Pick<SWRResponse<Data, Error>, 'error'> {
-  data: Timeline<Data> | undefined;
+  data: TimelineData<Data> | undefined;
   isValidating: boolean;
   isLoading: boolean;
-  mutateLatest: (data?: Timeline<Data>) => Promise<Timeline<Data> | undefined>;
-  mutateOlder: (data?: Data[]) => Promise<Timeline<Data> | undefined>;
-  mutateGap: (gap: Gap<Data>) => Promise<Timeline<Data> | undefined>;
+  mutateLatest: () => Promise<TimelineData<Data> | undefined>;
+  mutateOlder: () => Promise<TimelineData<Data> | undefined>;
+  mutateGap: (gap: Gap<Data>) => Promise<TimelineData<Data> | undefined>;
+  queue: TimelineData<Data> | undefined;
 }
 
-export type SWRTimelineConfiguration<Data, Error> = Pick<
-  FullConfiguration<Data, Error>,
-  | 'revalidateOnFocus'
-  | 'revalidateOnReconnect'
-  | 'revalidateIfStale'
-  | 'shouldRetryOnError'
-  | 'errorRetryInterval'
-  | 'focusThrottleInterval'
-  | 'dedupingInterval'
-  | 'loadingTimeout'
-  | 'cache'
-  | 'fallback'
-  | 'use'
->;
+export interface SWRTimelineConfiguration<Data = any, Error = any>
+  extends Omit<SWRConfiguration<Data, Error>, 'compare'> {
+  enableQueue?: boolean;
+}
 
 export interface SWRTimelineHook {
-  <Data = any, Error = any>(key: Key): SWRTimelineResponse<Data, Error>;
-
-  <Data = any, Error = any>(
-    key: Key,
-    fetcher: SWRTimelineFetcher<Data> | null,
+  <Data = any, Error = any, SWRKey extends Arguments = Arguments>(
+    getKey: SWRTimelineKeyLoader<Data, SWRKey>,
+    fetcher: SWRTimelineFetcher<Data, SWRKey> | null,
   ): SWRTimelineResponse<Data, Error>;
 
-  // いずれConfigを使うときに機能を絞り追加する
-  // <Data = any, Error = any>(
-  //   key: Key,
-  //   fetcher: SWRTimelineFetcher<Data> | null,
-  //   config?: SWRTimelineConfiguration<Data, Error>,
-  // ): SWRTimelineResponse<Data, Error>;
+  <Data = any, Error = any, SWRKey extends Arguments = Arguments>(
+    getKey: SWRTimelineKeyLoader<Data, SWRKey>,
+    fetcher: SWRTimelineFetcher<Data, SWRKey> | null,
+    config: SWRTimelineConfiguration<Data, Error>,
+  ): SWRTimelineResponse<Data, Error>;
 }
 
-export interface SWRTimelineCacheValue<Data, Error = any>
-  extends State<Timeline<Data>, Error> {
+export interface TimelineDataCacheValue<
+  Data,
+  Error,
+  SWRKey extends Arguments = Arguments,
+> extends State<TimelineData<Data>, Error> {
+  // key
+  _k?: Arguments;
+  // Keys
+  _keys?: SWRKey[];
+  // mutate trigger
+  _trigger?: 'older' | 'latest' | 'gap';
+}
+
+export interface ResultDataCacheValue<Data = never, Error = never>
+  extends State<ResultData<Data>, Error> {
   // key
   _k?: Arguments;
 }
