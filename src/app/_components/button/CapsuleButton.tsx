754b94500c6a6f5904f73f335001659e96f28c79
takecchi <takecchi.kobayashi@gmail.com>
2023-11-23 15:42:25 +0900

    CapsuleButtonからshadowを削除

--- src/app/_components/button/CapsuleButton.tsx
+++ src/app/_components/button/CapsuleButton.tsx
@@ -4,6 +4,12 @@ import { styled, Button } from '@mui/material';
 
 const CapsuleButton = styled(Button)`
   border-radius: 9999px;
+  box-shadow: none;
+
+  &:hover,
+  &:focus {
+    box-shadow: none;
+  }
 `;
 
 export default CapsuleButton;
