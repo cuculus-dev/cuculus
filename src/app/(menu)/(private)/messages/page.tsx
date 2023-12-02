78f10e8b17d056d1ed4a7aa56d4a582514257d30
takecchi <takecchi.kobayashi@gmail.com>
2023-12-03 00:05:45 +0900

    ComingSoon使用箇所の修正

--- src/app/(menu)/(private)/messages/page.tsx
+++ src/app/(menu)/(private)/messages/page.tsx
@@ -1,9 +1,12 @@
 import ComingSoon from '@/app/(menu)/_components/main/ComingSoon';
+import PrimaryColumn from '@/app/(menu)/_components/main/PrimaryColumn';
 
 export default function page() {
   return (
     <main>
-      <ComingSoon />
+      <PrimaryColumn hideHeader={true}>
+        <ComingSoon />
+      </PrimaryColumn>
     </main>
   );
 }
