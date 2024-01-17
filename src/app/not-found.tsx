02fc13821e2746b4ab3954afa8f0189819737d06
takecchi <takecchi.kobayashi@gmail.com>
2024-01-17 20:21:49 +0900

    404 pages移行準備

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,15 @@
 import NotFound from '@/app/_components/NotFound';
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'ページが見つかりません',
-};
+// export const metadata: Metadata = {
+//   title: 'ページが見つかりません',
+// };
 
-export default function page() {
+const Page = () => {
   return (
     <main style={{ height: '100vh' }}>
       <NotFound />
     </main>
   );
-}
+};
+
+export default Page;
