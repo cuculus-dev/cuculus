f352c6b08a19f1900b45ab482665158b7988df16
takecchi <takecchi.kobayashi@gmail.com>
2024-02-15 01:53:53 +0900

    汎用的な型に修正

--- src/app/(menu)/_utils/post.utils.ts
+++ src/app/(menu)/_utils/post.utils.ts
@@ -1,10 +1,13 @@
-import { UserPost } from '@cuculus/cuculus-api';
+type BasePost = {
+  originalPostId?: string;
+  text?: string;
+};
 
 /**
  * リポストか否か
  * @param post
  */
-export function isRepost(post: UserPost): boolean {
+export function isRepost(post: BasePost): boolean {
   return post.originalPostId !== undefined && !post.text;
 }
 
@@ -12,6 +15,6 @@ export function isRepost(post: UserPost): boolean {
  * 引用リポストか否か
  * @param post
  */
-export function isQuotedRepost(post: UserPost): boolean {
+export function isQuotedRepost(post: BasePost): boolean {
   return post.originalPostId !== undefined && post.text !== undefined;
 }
