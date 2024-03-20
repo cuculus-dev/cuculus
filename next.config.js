53437410eb2fa33758db88c64696ab0a92d2109b
takecchi <takecchi.kobayashi@gmail.com>
2024-03-21 03:16:54 +0900

    @next/bundle-analyzerの導入 (#374)
    ## Issue
    
    - Github Issue: #231 
    
    ## 変更内容
    - `npm run analyze:win` または `npm run analyze`でバンドルJSの分析が出来るように
    
    @coderabbitai: ignore

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
 });
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -50,4 +53,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = withBundleAnalyzer(withPWA(nextConfig));
