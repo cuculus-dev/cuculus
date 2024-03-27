71190c9c862dc6ef45ecaf394098cdfb25ae4fcd
ayumu akimoto <akki.ayumu.akimoto@gmail.com>
2024-03-27 14:57:35 +0900

    レイアウト調整

--- src/app/(plain)/(public)/privacy/_components/PrivacyPolicy.tsx
+++ src/app/(plain)/(public)/privacy/_components/PrivacyPolicy.tsx
@@ -11,15 +11,15 @@ const Root = styled('div')`
   justify-content: center;
   align-content: center;
   flex-direction: column;
-  width: 100%;
+  max-width: 100%;
   height: 100%;
 `;
 
 const Container = styled('div')`
   display: flex;
   flex-direction: column;
-  gap: 16px;
-  text-align: center;
+  max-width: 60%;
+  margin: 0 auto;
 `;
 
 const Title = styled('div')`
@@ -28,19 +28,22 @@ const Title = styled('div')`
   line-height: 2.5rem;
 `;
 
+const ButtonAlea = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`;
 export default function NotFound() {
   return (
     <Root>
       <Container>
-        <Title className={museoModerno.className}></Title>
         <div>
-          <h1>Cuculusプライバシーポリシー</h1>
-          <h2>1. はじめに</h2>
+          <h3>1. はじめに</h3>
           <p>
             Cuculus（以下、当サイト）は、ユーザーのプライバシーを尊重し、個人情報の保護に努めます。
           </p>
 
-          <h2>2. 収集する情報</h2>
+          <h3>2. 収集する情報</h3>
           <p>当サイトでは、以下の情報を収集することがあります。</p>
           <ul>
             <li>
@@ -49,7 +52,7 @@ export default function NotFound() {
             <li>利用状況に関する情報（例：ログデータ、アクセスIPアドレス）</li>
           </ul>
 
-          <h2>3. 情報の利用目的</h2>
+          <h3>3. 情報の利用目的</h3>
           <p>収集した情報は以下の目的で利用されます。</p>
           <ul>
             <li>当サイトの提供・改善</li>
@@ -58,7 +61,7 @@ export default function NotFound() {
             <li>新機能やサービスの案内</li>
           </ul>
 
-          <h2>4. 情報の共有と開示</h2>
+          <h3>4. 情報の共有と開示</h3>
           <p>
             当サイトは、以下の場合を除き、ユーザーの個人情報を第三者に開示または共有しません。
           </p>
@@ -68,12 +71,12 @@ export default function NotFound() {
             <li>ユーザーの権利や安全を守るため必要な場合</li>
           </ul>
 
-          <h2>5. 情報の保護</h2>
+          <h3>5. 情報の保護</h3>
           <p>
             当サイトは、収集した情報の安全を確保するために、適切な物理的、技術的、組織的対策を講じます。
           </p>
 
-          <h2>6. ユーザーの権利</h2>
+          <h3>6. ユーザーの権利</h3>
           <p>ユーザーは、自己の個人情報に関して、以下の権利を有します。</p>
           <ul>
             <li>アクセス権</li>
@@ -82,20 +85,22 @@ export default function NotFound() {
             <li>処理制限の権利</li>
           </ul>
 
-          <h2>7. プライバシーポリシーの変更</h2>
+          <h3>7. プライバシーポリシーの変更</h3>
           <p>
             本ポリシーは、必要に応じて更新されることがあります。本ポリシーの内容は、ユーザーに通知することなく、変更することができるものとします。
           </p>
 
-          <h2>8. お問い合わせ</h2>
+          <h3>8. お問い合わせ</h3>
           <p>
             本プライバシーポリシーに関するご質問や懸念がある場合は、こちらよりご連絡ください。
           </p>
         </div>
+      </Container>
+      <ButtonAlea>
         <LinkButton href={'/'} variant={'outlined'} aria-label="トップに戻る">
           トップに戻る
         </LinkButton>
-      </Container>
+      </ButtonAlea>
     </Root>
   );
 }
